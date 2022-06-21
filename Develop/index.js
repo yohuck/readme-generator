9f150d3b4d34ce2f0b8fd224845eaa08a3c39d8c
Eric Lake <ericallenlake@gmail.com>
2022-06-21 15:00:21 -0600

    Links file system and sets writeToFile function

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,16 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
-const generateMarkdown = require('./utils/generateMarkdown')
+const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = ['What is the name of your project?', "Description: What is your motivation to build this project?"];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.appendFile(`${fileName}.md`, `${data}`, (err) => {
+        err? console.error(err) : console.log('Success!')
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {}
@@ -15,3 +20,5 @@ init();
 
 generateMarkdown.renderLicenseBadge('hello world')
 generateMarkdown.renderLicenseLink('oh no')
+
+writeToFile('test', '# This is a test')
