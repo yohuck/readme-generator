4cb422eb02dd035c003defcf9d26d225383c3c7f
Yo Huck <ericallenlake@gmail.com>
2022-06-20 22:29:21 -0600

    Links imports

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,8 @@
 // TODO: Include packages needed for this application
-
+const batman = require('inquirer')
+const generateMarkdown = import {'generateMarkdown' } from "./utils/generateMarkdown";
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the name of your project?', "Description: What is your motivation to build this project?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
