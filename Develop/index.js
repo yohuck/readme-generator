07a91b96a9e91e0f1a14fddd1ca3dc5962763096
Eric Lake <ericallenlake@gmail.com>
2022-06-22 14:18:04 -0600

    Adds all required questions for inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -11,9 +11,46 @@ const questions = [
     }, 
     {
         type: 'input',
-        name: 'motivation',
-        message: "Description: What is your motivation to build this project?"
-    }];
+        name: 'description',
+        message: "Description: Enter a description of your project"
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: "Installation: Enter the steps to install your project"
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "Usage: Enter the usage details for your project"
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: "Contribution: Enter the contribution guidelines for your project"
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: "Testing: Enter the testing instructions for your project"
+    },
+    {
+        type: 'list',
+        message: 'What type of license does your project use?',
+        name: 'license',
+        choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'HNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+];
     
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
